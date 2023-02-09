82a7beef4d69d6fc9813c06db98b03d8fbaa08a3
Ian Silva <ian.silva@verity.com.br>
2023-02-09 14:36:49 -0300

    fix: impro charts

--- web/sections/NewLayout/LineChartNew/index.js
+++ web/sections/NewLayout/LineChartNew/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Typography, Box } from '@mui/material'
+import { Grid, Typography, Box, MenuItem, Menu, Button } from '@mui/material'
 import { CSVLink } from 'react-csv'
+import * as XLSX from 'xlsx'
 import SimpleBlockContent from '../../../components/OldLayout/SimpleBlockContent'
 import axios from 'axios'
 import { format } from 'date-fns'
 import styles from './styles.module.scss'
 import { TfiDownload } from 'react-icons/tfi'
+import { RiFileExcel2Line, RiTable2 } from 'react-icons/ri'
 import { ResponsiveLine } from '@nivo/line'
 
 function LineChart(props) {
@@ -25,6 +27,16 @@ function LineChart(props) {
   const colors = [chartColor ? chartColor : '#0082E5', '#DC6E19', '#869D7A', '#FF2205']
 
   const [data, setData] = React.useState()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const convertDate = (value) => {
     const getLocale = (locale) => require(`date-fns/locale/${locale}/index.js`)
@@ -43,7 +55,31 @@ function LineChart(props) {
   const limitLabel = (text) => {
     let count = 6
     let textLength = text.toString().length
-    return text.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,').slice(0, count) + (textLength > count ? "..." : "");
+    let newText = desktopSize === 12 ? text.toFixed(0) : text.toFixed(2)
+    return newText.replace(/\d(?=(\d{3})+\.)/g, '$&,').slice(0, count) + (textLength > count ? "..." : "");
+  }
+
+  function downloadExcel() {
+    const worksheet = XLSX.utils.json_to_sheet(data)
+    const workbook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+    XLSX.writeFile(workbook, `line-chart-${_id}.xlsx`)
+    handleClose()
+  }
+
+  const submenuStyle = {
+    //width: '160px',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    fontFamily: 'var(--font-family-primary)',
+    fontSize: 'var(--font-size-secondary-sm)',
+    color: 'var(--black)',
+    alignItems: 'center',
+    margin: '0px 28.5px',
+    '&:hover': {
+      background: 'none'
+    }
   }
 
   React.useEffect(() => {
@@ -69,7 +105,7 @@ function LineChart(props) {
         datasets.push({
           id: item.label,
           color: colors[count],
-          data: newData.sort((a, b) =>  new Date(a.x) - new Date(b.x)),
+          data: newData.sort((a, b) => new Date(a.x) - new Date(b.x)),
         })
         count = count + 1
         return null
@@ -87,9 +123,9 @@ function LineChart(props) {
               variant="h2"
               sx={{
                 fontFamily: 'var(--font-family-primary)',
-                fontSize: 'var(--font-size-primary-lg)',
+                fontSize: { xs: 'var(--font-size-primary-md)', md: desktopSize === 12 ? 'var(--font-size-primary-lg)' : 48 },
                 color: 'var(--black)',
-                minHeight: {md: desktopSize === 6 && '200px'}
+                minHeight: { md: desktopSize === 6 && '200px' }
               }}
             >
               {heading}
@@ -98,19 +134,24 @@ function LineChart(props) {
         )}
         {data && (
           <Grid item xs={12} mb={4} sx={{ display: 'flex', justifyContent: 'flex-end', gap: 1 }}>
-            <CSVLink
-              data={data}
-              filename={`line-chart-${_id}.csv`}
-              target="_blank"
-              style={{
+            <Button
+              id="demo-positioned-button"
+              aria-controls={open ? 'demo-positioned-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{
+                fontFamily: 'var(--font-family-primary)',
                 textAlign: 'center',
                 background: 'transparent',
-                border: '2px solid #091B3F',
-                color: '#091B3F',
+                border: '2px solid var(--black)',
+                color: 'var(--black)',
                 textDecoration: 'none',
                 padding: '5px 25px',
-                borderRadius: '4px',
-                fontSize: '20px'
+                borderRadius: open ? '4px 4px 0px 0px' : '4px',
+                fontSize: 'var(--font-size-secondary-sm)',
+                textTransform: 'initial',
+                borderBottom: open && 'none'
               }}
             >
               <TfiDownload
@@ -118,7 +159,63 @@ function LineChart(props) {
                 className={styles.download__icon}
               />
               Download
-            </CSVLink>
+            </Button>
+            <Menu
+              id={`menu-${_id}`}
+              aria-labelledby="demo-positioned-button"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              sx={{
+                "& .MuiPaper-root": {
+                  backgroundColor: "var(--background-color)",
+                  boxShadow: 'none',
+                  borderRadius: '0px 0px 4px 4px',
+                  borderLeft: '2px solid var(--black)',
+                  borderRight: '2px solid var(--black)',
+                  borderBottom: '2px solid var(--black)',
+                }
+              }}
+            >
+              <MenuItem onClick={() => downloadExcel()} sx={{ borderTop: '1px solid #ececec', ...submenuStyle }}>
+                <RiFileExcel2Line
+                  size={17}
+                  className={styles.download__icon}
+                />
+                Excel
+              </MenuItem>
+              <MenuItem onClick={handleClose} sx={submenuStyle}>
+                <CSVLink
+                  data={data}
+                  onClick={handleClose}
+                  filename={`line-chart-${_id}.csv`}
+                  target="_blank"
+                  style={{
+                    textAlign: 'center',
+                    background: 'transparent',
+                    color: 'var(--black)',
+                    textDecoration: 'none',
+                    fontSize: 'var(--font-size-secondary-sm)',
+                    display: 'flex',
+                    alignItems: 'center'
+                  }}
+                >
+                  <RiTable2
+                    size={18}
+                    className={styles.download__icon}
+                  />
+                  CSV
+                </CSVLink>
+              </MenuItem>
+            </Menu>
           </Grid>
         )}
         <Grid item xs={12}>
@@ -132,7 +229,7 @@ function LineChart(props) {
                 <ResponsiveLine
                   colors={{ datum: 'color' }}
                   data={data && dataSet(data)}
-                  margin={{ top: 50, right: 50, bottom: 50, left: 70 }}
+                  margin={{ top: 50, right: 50, bottom: 70, left: 80 }}
                   xScale={{
                     type: "time",
                     format: "%Y-%m-%d",
@@ -160,6 +257,11 @@ function LineChart(props) {
                   pointBorderColor={{ from: 'serieColor' }}
                   pointLabelYOffset={-12}
                   useMesh={true}
+                  theme={{
+                    textColor: 'var(--black)',
+                    fontSize: 12,
+                    fontFamily: 'var(--font-family-secondary)',
+                  }}
                   legends={[
                     {
                       anchor: 'top-left',
@@ -167,7 +269,7 @@ function LineChart(props) {
                       justify: false,
                       translateX: 0,
                       translateY: 0,
-                      itemsSpacing: 40,
+                      itemsSpacing: 100,
                       itemWidth: 80,
                       itemHeight: -50,
                       itemOpacity: 0.75,
