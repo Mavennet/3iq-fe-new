76a5b91e76b961569c9bd590cdfa5555de454898
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-02-22 12:18:37 -0500

    Change icon

--- web/sections/NewLayout/MainHero/index.js
+++ web/sections/NewLayout/MainHero/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { PropTypes } from 'prop-types'
 import styles from './styles.module.scss'
 import { Container, Grid, Box, useMediaQuery, useTheme } from '@mui/material'
-import { FaTwitter, FaYoutube, FaLinkedin, FaEthereum, FaMixer } from 'react-icons/fa'
+import { FaTwitter, FaYoutube, FaLinkedin, FaEthereum, FaRegHandshake } from 'react-icons/fa'
 import { BsCurrencyBitcoin } from 'react-icons/bs'
 import Link from 'next/link'
 import Button from '../../../components/NewLayout/Button'
@@ -144,7 +144,7 @@ function MainHero(props) {
             <a>
               <div className={styles.box__main__hero}>
                 <div className={styles.box__title}>
-                  <FaMixer />
+                  <FaRegHandshake />
                   {thirdBoxTitle && <h5>{thirdBoxTitle}</h5>}
                 </div>
                 {thirdBoxDescription && <p>{thirdBoxDescription}</p>}
