48b87cb8bd4927d0fab831857f1dea9285bdfa1a
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-03-16 16:16:31 -0400

    Revert "Resolve conflicts"
    This reverts commit 51b6d2008daa4aaca88913b19566fc6bac661f43.

--- web/sections/OldLayout/ReadMoreCard/index.js
+++ web/sections/OldLayout/ReadMoreCard/index.js
@@ -7,10 +7,9 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import {createTheme, ThemeProvider} from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
-import RedirectButton from '../../../components/OldLayout/RedirectButton'
-import SimpleBlockContent from '../../../components/OldLayout/SimpleBlockContent'
-import styles from './styles.module.scss'
-import {Container} from '@mui/material'
+ import RedirectButton from '../../../components/OldLayout/RedirectButton'
+ import SimpleBlockContent from '../../../components/OldLayout/SimpleBlockContent'
+import styles from'./styles.module.scss'
 
 const theme = createTheme({
   typography: {
@@ -20,12 +19,12 @@ const theme = createTheme({
     },
     h2: {
       fontSize: 20,
-      fontWeight: 'bold',
+      fontWeight: 'bold'
     },
     h5: {
       fontSize: 20,
-      fontWeight: 'bold',
-    },
+      fontWeight: 'bold'
+    }
   },
 })
 
@@ -34,85 +33,73 @@ function urlFor(source) {
 }
 
 function ReadMoreCard(props) {
-  const {button, heading, backgroundImage, description, currentLanguage} = props
+  const {button, heading, backgroundImage, description, currentLanguage } = props
 
   const localeButton = button[currentLanguage?.languageTag]
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container disableGutters sx={{maxWidth: {sm: 'xl'}}}>
-        <Grid container component="main">
-          <Grid
-            item
-            xs={12}
-            py={{xs: 20}}
-            sm={4}
-            md={6}
+      <Grid container component="main">
+        <CssBaseline />
+        <Grid
+          item
+          xs={12}
+          py={{xs: 40}}
+          sm={4}
+          md={6}
+          sx={{
+            background:
+              backgroundImage.asset._ref && `url("${urlFor(backgroundImage.asset._ref).url()}") no-repeat center center`,
+            backgroundSize: 'cover',
+            bgcolor: '#091b3f',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+        </Grid>
+        <Grid item xs={12} sm={8} md={6} elevation={6} square>
+          <Box
             sx={{
-              background:
-                backgroundImage.asset._ref &&
-                `url("${urlFor(backgroundImage.asset._ref).url()}") no-repeat center center`,
-              backgroundSize: 'cover',
-              bgcolor: '#091b3f',
+              mt: 10,
+              mb: 2,
+              ml: {xs: 2, md: 10},
+              mr: {xs: 0, md: 8},
               display: 'flex',
               flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
+              alignItems: 'left',
+              color: '#091b3f',
             }}
-          ></Grid>
-          <Grid item xs={12} sm={8} md={6} elevation={6} square>
+          >
+            {heading && (
+              <Typography component="h1" variant="h4" style={{fontWeight: 'bold', color: '#0082E5'}} gutterBottom>
+                {heading}
+              </Typography>
+            )}
+            {description && (
+              <div className={styles.simpleBlockContent}>
+                <SimpleBlockContent blocks={description} />
+              </div>
+            )}
             <Box
               sx={{
-                mt: 10,
-                mb: 2,
-                ml: {xs: 2, md: 10},
-                mr: {xs: 0, md: 10},
+                mt: 5,
+                mb: 5,
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'left',
-                color: '#091b3f',
+                justifyContent: {xs: 'center', md: 'flex-end'}
               }}
             >
-              {heading && (
-                <Typography
-                  component="h1"
-                  variant="h4"
-                  style={{fontWeight: 'bold', color: '#0082E5'}}
-                >
-                  {heading}
-                </Typography>
-              )}
-              {description && (
-                <div className={styles.simpleBlockContent}>
-                  <SimpleBlockContent blocks={description} />
-                </div>
-              )}
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: {xs: 'center', md: 'flex-end'},
-                }}
-              >
-                {localeButton && (localeButton.route || localeButton.link) && (
-                  <RedirectButton
-                    {...localeButton}
-                    sx={{
-                      width: {xs: 'auto', md: 180},
-                      padding: '10px 20px',
-                      margin: {xs: '0 14px 0 0', md: '0'},
-                      fontSize: '16px',
-                      background: '#091B3F',
-                      borderColor: '#091B3F',
-                      color: '#fff',
-                    }}
-                  />
-                )}
-              </Box>
+            {localeButton && (localeButton.route || localeButton.link) && (
+              <RedirectButton
+                {...localeButton}
+                sx={{width: {xs: 'auto', md: 180}, padding: '10px 20px', margin: {xs: '0 14px 0 0', md: '0'}, fontSize: '16px', background: '#091B3F', borderColor: '#091B3F', color: '#fff'}}
+              />
+            )}
             </Box>
-          </Grid>
+          </Box>
         </Grid>
-      </Container>
+      </Grid>
     </ThemeProvider>
   )
 }
