bc26cd2bf16ea96e74bf98dbaabc64c3ccf434f3
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-11-08 11:21:30 -0500

    feat: add meta pixel code

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -23,7 +23,25 @@ export default class MyDocument extends Document {
                 })(window,document,'script','dataLayer','GTM-P2CW2MZB');`,
             }}
           />
-          <link rel="icon" href="https://3iq-webdocs.s3.ca-central-1.amazonaws.com/3iq_fav_icon.ico"/>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1083325839775076');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <link
+            rel="icon"
+            href="https://3iq-webdocs.s3.ca-central-1.amazonaws.com/3iq_fav_icon.ico"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
